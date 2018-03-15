64658e63045ae26e07c6580cb7806611e7a9d5ae
brandon maldonado alonso <brandonmaldonadoalonso@MacBook-Pro-de-brandon.local>
2018-03-15 11:38:01 -0600

    Bug fixes

--- SwifterUI/SwifterUI/LoginKit/Signup/SFSignupView.swift
+++ SwifterUI/SwifterUI/LoginKit/Signup/SFSignupView.swift
@@ -93,10 +93,10 @@ open class SFSignupView: SFScrollView {
         lastNameSection.clipTop(to: .bottom, of: nameSection, margin: 16)
         
         mailSection.clipEdges(margin: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16), exclude: [.top, .bottom])
-        mailSection.clipTop(to: .bottom, of: mailSection, margin: 16)
+        mailSection.clipTop(to: .bottom, of: lastNameSection, margin: 16)
         
         passwordSection.clipEdges(margin: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16), exclude: [.top, .bottom])
-        passwordSection.clipTop(to: .bottom, of: lastNameSection, margin: 16)
+        passwordSection.clipTop(to: .bottom, of: mailSection, margin: 16)
         
         signUpButton.clipEdges(margin: UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16), exclude: [.top, .bottom])
         signUpButton.clipTop(to: .bottom, of: passwordSection, margin: 16)

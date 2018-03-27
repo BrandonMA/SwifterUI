6d3c5f9aa1fa49781f17368a81dcef757dbb3876
brandon maldonado alonso <brandonmaldonadoalonso@MacBook-Pro-de-brandon-maldonado-alonso.local>
2018-03-26 20:00:34 -0600

    Bug fixes

--- SwifterUI/SwifterUI/Firebase/FirebaseLogin.swift
+++ SwifterUI/SwifterUI/Firebase/FirebaseLogin.swift
@@ -1,35 +1,35 @@
-////
-////  LoginManager.swift
-////
-////  Created by brandon maldonado alonso on 24/02/18.
-////  Copyright © 2018 brandon maldonado alonso. All rights reserved.
-////
 //
-//import UIKit
-//import FirebaseAuth
-//import PromiseKit
+//  LoginManager.swift
 //
-//public protocol FirebaseLogin {
+//  Created by brandon maldonado alonso on 24/02/18.
+//  Copyright © 2018 brandon maldonado alonso. All rights reserved.
 //
-//    // MARK: - Instance Methods
-//
-//    func handleFirebaseLogin(user: User?, error: Error?) -> Promise<User>
-//}
-//
-//public extension FirebaseLogin {
-//
-//    // MARK: - Instance Methods
-//
-//    public func handleFirebaseLogin(user: User?, error: Error?) -> Promise<User> {
-//
-//        return Promise { seal in
-//
-//            DispatchQueue.addAsyncTask(to: .background, handler: {
-//                seal.resolve(user, error)
-//            })
-//
-//        }
-//
-//    }
-//}
+
+import UIKit
+import Firebase
+import PromiseKit
+
+public protocol FirebaseLogin {
+
+    // MARK: - Instance Methods
+
+    func handleFirebaseLogin(user: User?, error: Error?) -> Promise<User>
+}
+
+public extension FirebaseLogin {
+
+    // MARK: - Instance Methods
+
+    public func handleFirebaseLogin(user: User?, error: Error?) -> Promise<User> {
+
+        return Promise { seal in
+
+            DispatchQueue.addAsyncTask(to: .background, handler: {
+                seal.resolve(user, error)
+            })
+
+        }
+
+    }
+}
 
